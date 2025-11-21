bf77c66b8b4bd9f009bc9f3eaa23923bf108a81a
Josh Samuelson <josh@joshsamuelson.com>
2025-11-20 17:53:02 -0800

    refactor to use variables

--- variables.tf
+++ variables.tf
@@ -3,3 +3,38 @@ variable "instance_type" {
   default     = "t3.nano"
 
 }
+
+variable "ami_filter" {
+  description = "Name filter and owner for AMI"
+  type = object ({
+    name  = string
+    owner = string
+  })
+
+  default = {
+    name  = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+    owner = "979382823631" # Bitnami
+  }
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type = object ({
+    name           = string
+    network_prefix = string
+  })
+  default = {
+    name           = "dev"
+    network_prefix = "10.0"
+  }
+}
+
+variable "min_size"{
+  description = "Minimum number of instances in the ASG"
+  default = 1
+}
+
+variable "max_size"{
+  description = "Maximum number of instances in the ASG"
+  default = 2
+}
